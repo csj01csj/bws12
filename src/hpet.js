208fab061a44b3a4a88c9eedafe7107df4079ca2
a <92509728+operator0225@users.noreply.github.com>
2026-06-17 15:36:32 +0900

    Fix hpet, ioapic, kernel with full v86 content; add lib.js

--- src/hpet.js
+++ src/hpet.js
@@ -8,3 +8,190 @@ var HPET_ADDR = 0xFED00000,
     HPET_COUNTER_CONFIG = 1 << 4 | HPET_SUPPORT_64 << 5,
     HPET_COUNTER_CONFIG_MASK = 1 << 4 | 1 << 5 | 1 << 15,
     HPET_NUM_COUNTERS = 4;
+
+/**
+ * HPET - High Precision Event Timer
+ * http://wiki.osdev.org/HPET
+ *
+ * @constructor
+ * @param {CPU} cpu
+ */
+function HPET(cpu)
+{
+    var me = this,
+
+        hpet_enabled = false,
+        hpet_start = Date.now(),
+
+        hpet_offset_low = 0,
+        hpet_offset_high = 0,
+
+        counter_read_acc_next = false,
+        interrupt_status = 0,
+
+        counter_config = new Int32Array(HPET_NUM_COUNTERS << 1),
+        counter_comparator = new Int32Array(HPET_NUM_COUNTERS << 1),
+        counter_accumulator = new Int32Array(HPET_NUM_COUNTERS << 1);
+
+    var last_check = 0;
+
+    this.legacy_mode = false;
+
+    this.timer = function(now)
+    {
+        if(!hpet_enabled) return 100;
+
+        var counter_value = get_counter() >>> 0, config, comparator, do_irq;
+
+        for(var i = 0; i < HPET_NUM_COUNTERS; i++)
+        {
+            config = counter_config[i << 1];
+            comparator = counter_comparator[i << 1] >>> 0;
+
+            if(last_check <= counter_value ?
+                    comparator > last_check && comparator <= counter_value :
+                    comparator > last_check || comparator <= counter_value
+            ) {
+                do_irq = config & 4;
+
+                if(config & 2)
+                {
+                    do_irq = do_irq && !(interrupt_status & 1 << i);
+                    interrupt_status |= 1 << i;
+                }
+                else
+                {
+                    interrupt_status &= ~(1 << i);
+                }
+
+                if(config & 1 << 3)
+                {
+                    counter_comparator[i << 1] += counter_accumulator[i << 1];
+                }
+
+                if(do_irq)
+                {
+                    if(me.legacy_mode && i === 0) cpu.device_raise_irq(0);
+                    else if(me.legacy_mode && i === 1) cpu.device_raise_irq(0);
+                    else cpu.device_raise_irq(0); // TODO
+                }
+            }
+        }
+
+        last_check = counter_value;
+        return 100;
+    };
+
+    function get_counter()
+    {
+        if(hpet_enabled) return (Date.now() - hpet_start) * HPET_FREQ_MS + hpet_offset_low | 0;
+        else return hpet_offset_low;
+    }
+
+    function get_counter_high()
+    {
+        if(HPET_SUPPORT_64)
+        {
+            if(hpet_enabled) return (Date.now() - hpet_start) * (HPET_FREQ_MS / 0x100000000) + hpet_offset_high | 0;
+            else return hpet_offset_high;
+        }
+        else return 0;
+    }
+
+    cpu.io.mmap_register(HPET_ADDR, 0x4000, mmio_read, mmio_write);
+
+    function mmio_read(addr)
+    {
+        dbg_log("Read " + h(addr, 4) + " (ctr=" + h(get_counter() >>> 0) + ")", LOG_HPET);
+
+        switch(addr)
+        {
+            case 0: return 1 << 16 | HPET_NUM_COUNTERS - 1 << 8 | 0x8000 | 0x01 | HPET_SUPPORT_64 << 13;
+            case 4: return HPET_PERIOD;
+            case 0x10: return me.legacy_mode << 1 | hpet_enabled;
+            case 0xF0: return get_counter();
+            case 0xF4: return get_counter_high();
+        }
+
+        var register = addr >> 2 & 7, counter = addr - 0x100 >> 5;
+
+        if(addr < 0x100 || counter >= HPET_NUM_COUNTERS || register > 5)
+        {
+            dbg_log("Read reserved address: " + h(addr), LOG_HPET);
+            return 0;
+        }
+
+        switch(register)
+        {
+            case 0: return counter_config[counter << 1] & ~HPET_COUNTER_CONFIG_MASK | HPET_COUNTER_CONFIG;
+            case 1: return counter_config[counter << 1 | 1];
+            case 2: return counter_comparator[counter << 1];
+            case 3: return counter_comparator[counter << 1 | 1];
+            case 4:
+            case 5: return 0;
+        }
+    }
+
+    function mmio_write(addr, data)
+    {
+        dbg_log("Write " + h(addr, 4) + ": " + h(data, 2), LOG_HPET);
+
+        switch(addr)
+        {
+            case 0x10:
+                if((hpet_enabled ^ data) & 1)
+                {
+                    if(data & 1) hpet_start = Date.now();
+                    else { hpet_offset_low = get_counter(); hpet_offset_high = get_counter_high(); }
+                }
+                hpet_enabled = (data & 1) === 1;
+                me.legacy_mode = (data & 2) === 2;
+                return;
+            case 0x20:
+                interrupt_status &= ~data;
+                return;
+            case 0xF0:
+                hpet_offset_low = data;
+                return;
+            case 0xF4:
+                hpet_offset_high = data;
+                return;
+        }
+
+        var register = addr >> 2 & 7, counter = addr - 0x100 >> 5;
+
+        if(addr < 0x100 || counter >= HPET_NUM_COUNTERS || register > 2)
+        {
+            dbg_log("Write reserved address: " + h(addr) + " data=" + h(data), LOG_HPET);
+            return;
+        }
+
+        switch(register)
+        {
+            case 0:
+                counter_config[counter << 1] = data;
+                break;
+            case 1:
+                break;
+            case 2:
+                if(counter_read_acc_next)
+                {
+                    counter_accumulator[counter << 1] = data;
+                    counter_read_acc_next = false;
+                }
+                else
+                {
+                    counter_comparator[counter << 1] = data;
+                    if(counter_config[counter << 1] & 1 << 6)
+                    {
+                        counter_read_acc_next = true;
+                        counter_config[counter << 1] &= ~(1 << 6);
+                    }
+                }
+                break;
+            case 3:
+                counter_comparator[counter << 1 | 1] = data;
+                break;
+        }
+    }
+}
--- src/kernel.js
+++ src/kernel.js
@@ -1,3 +1,176 @@
 "use strict";
 
-// kernel loader placeholder
+// https://www.kernel.org/doc/Documentation/x86/boot.txt
+
+const LINUX_BOOT_HDR_SETUP_SECTS = 0x1F1;
+const LINUX_BOOT_HDR_SYSSIZE = 0x1F4;
+const LINUX_BOOT_HDR_VIDMODE = 0x1FA;
+const LINUX_BOOT_HDR_BOOT_FLAG = 0x1FE;
+const LINUX_BOOT_HDR_HEADER = 0x202;
+const LINUX_BOOT_HDR_VERSION = 0x206;
+const LINUX_BOOT_HDR_TYPE_OF_LOADER = 0x210;
+const LINUX_BOOT_HDR_LOADFLAGS = 0x211;
+const LINUX_BOOT_HDR_CODE32_START = 0x214;
+const LINUX_BOOT_HDR_RAMDISK_IMAGE = 0x218;
+const LINUX_BOOT_HDR_RAMDISK_SIZE = 0x21C;
+const LINUX_BOOT_HDR_HEAP_END_PTR = 0x224;
+const LINUX_BOOT_HDR_CMD_LINE_PTR = 0x228;
+const LINUX_BOOT_HDR_INITRD_ADDR_MAX = 0x22C;
+const LINUX_BOOT_HDR_KERNEL_ALIGNMENT = 0x230;
+const LINUX_BOOT_HDR_RELOCATABLE_KERNEL = 0x234;
+const LINUX_BOOT_HDR_MIN_ALIGNMENT = 0x235;
+const LINUX_BOOT_HDR_XLOADFLAGS = 0x236;
+const LINUX_BOOT_HDR_CMDLINE_SIZE = 0x238;
+const LINUX_BOOT_HDR_PAYLOAD_OFFSET = 0x248;
+const LINUX_BOOT_HDR_PAYLOAD_LENGTH = 0x24C;
+const LINUX_BOOT_HDR_PREF_ADDRESS = 0x258;
+const LINUX_BOOT_HDR_INIT_SIZE = 0x260;
+
+const LINUX_BOOT_HDR_CHECKSUM1 = 0xAA55;
+const LINUX_BOOT_HDR_CHECKSUM2 = 0x53726448;
+
+const LINUX_BOOT_HDR_TYPE_OF_LOADER_NOT_ASSIGNED = 0xFF;
+
+const LINUX_BOOT_HDR_LOADFLAGS_LOADED_HIGH = 1 << 0;
+const LINUX_BOOT_HDR_LOADFLAGS_QUIET_FLAG = 1 << 5;
+const LINUX_BOOT_HDR_LOADFLAGS_KEEP_SEGMENTS = 1 << 6;
+const LINUX_BOOT_HDR_LOADFLAGS_CAN_USE_HEAPS = 1 << 7;
+
+
+function load_kernel(mem8, bzimage, initrd, cmdline)
+{
+    dbg_log("Trying to load kernel of size " + bzimage.byteLength);
+
+    const KERNEL_HIGH_ADDRESS = 0x100000;
+    const INITRD_ADDRESS = 64 << 20;
+    const quiet = false;
+
+    const bzimage8 = new Uint8Array(bzimage);
+    const bzimage16 = new Uint16Array(bzimage);
+    const bzimage32 = new Uint32Array(bzimage);
+
+    const setup_sects = bzimage8[LINUX_BOOT_HDR_SETUP_SECTS] || 4;
+    const syssize = bzimage32[LINUX_BOOT_HDR_SYSSIZE >> 2] << 4;
+    const vidmode = bzimage16[LINUX_BOOT_HDR_VIDMODE >> 1];
+
+    const checksum1 = bzimage16[LINUX_BOOT_HDR_BOOT_FLAG >> 1];
+    if(checksum1 !== LINUX_BOOT_HDR_CHECKSUM1) { dbg_log("Bad checksum1: " + h(checksum1)); return; }
+
+    const checksum2 = bzimage16[LINUX_BOOT_HDR_HEADER >> 1] | bzimage16[LINUX_BOOT_HDR_HEADER + 2 >> 1] << 16;
+    if(checksum2 !== LINUX_BOOT_HDR_CHECKSUM2) { dbg_log("Bad checksum2: " + h(checksum2)); return; }
+
+    const protocol = bzimage16[LINUX_BOOT_HDR_VERSION >> 1];
+    dbg_assert(protocol >= 0x202);
+
+    const flags = bzimage8[LINUX_BOOT_HDR_LOADFLAGS];
+    dbg_assert(flags & LINUX_BOOT_HDR_LOADFLAGS_LOADED_HIGH);
+
+    const flags2 = bzimage16[LINUX_BOOT_HDR_XLOADFLAGS >> 1];
+    const initrd_addr_max = bzimage32[LINUX_BOOT_HDR_INITRD_ADDR_MAX >> 2];
+    const kernel_alignment = bzimage32[LINUX_BOOT_HDR_KERNEL_ALIGNMENT >> 2];
+    const relocatable_kernel = bzimage8[LINUX_BOOT_HDR_RELOCATABLE_KERNEL];
+    const min_alignment = bzimage8[LINUX_BOOT_HDR_MIN_ALIGNMENT];
+    const cmdline_size = bzimage32[LINUX_BOOT_HDR_CMDLINE_SIZE >> 2];
+    const payload_offset = bzimage32[LINUX_BOOT_HDR_PAYLOAD_OFFSET >> 2];
+    const payload_length = bzimage32[LINUX_BOOT_HDR_PAYLOAD_LENGTH >> 2];
+    const pref_address = bzimage32[LINUX_BOOT_HDR_PREF_ADDRESS >> 2];
+    const pref_address_high = bzimage32[LINUX_BOOT_HDR_PREF_ADDRESS + 4 >> 2];
+    const init_size = bzimage32[LINUX_BOOT_HDR_INIT_SIZE >> 2];
+
+    const real_mode_segment = 0x8000;
+    const base_ptr = real_mode_segment << 4;
+    const heap_end = 0xE000;
+    const heap_end_ptr = heap_end - 0x200;
+
+    bzimage8[LINUX_BOOT_HDR_TYPE_OF_LOADER] = LINUX_BOOT_HDR_TYPE_OF_LOADER_NOT_ASSIGNED;
+
+    const new_flags =
+        (quiet ? flags | LINUX_BOOT_HDR_LOADFLAGS_QUIET_FLAG : flags & ~LINUX_BOOT_HDR_LOADFLAGS_QUIET_FLAG)
+        & ~LINUX_BOOT_HDR_LOADFLAGS_KEEP_SEGMENTS
+        | LINUX_BOOT_HDR_LOADFLAGS_CAN_USE_HEAPS;
+    bzimage8[LINUX_BOOT_HDR_LOADFLAGS] = new_flags;
+
+    bzimage16[LINUX_BOOT_HDR_HEAP_END_PTR >> 1] = heap_end_ptr;
+    bzimage16[LINUX_BOOT_HDR_VIDMODE >> 1] = 0xFFFF;
+
+    cmdline += "\x00";
+    dbg_assert(cmdline.length < cmdline_size);
+
+    const cmd_line_ptr = base_ptr + heap_end;
+    bzimage32[LINUX_BOOT_HDR_CMD_LINE_PTR >> 2] = cmd_line_ptr;
+    for(let i = 0; i < cmdline.length; i++)
+    {
+        mem8[cmd_line_ptr + i] = cmdline.charCodeAt(i);
+    }
+
+    const prot_mode_kernel_start = (setup_sects + 1) * 512;
+    const real_mode_kernel = new Uint8Array(bzimage, 0, prot_mode_kernel_start);
+    const protected_mode_kernel = new Uint8Array(bzimage, prot_mode_kernel_start);
+
+    let ramdisk_address = 0;
+    let ramdisk_size = 0;
+
+    if(initrd)
+    {
+        ramdisk_address = INITRD_ADDRESS;
+        ramdisk_size = initrd.byteLength;
+        dbg_assert(KERNEL_HIGH_ADDRESS + protected_mode_kernel.length < ramdisk_address);
+        mem8.set(new Uint8Array(initrd), ramdisk_address);
+    }
+
+    bzimage32[LINUX_BOOT_HDR_RAMDISK_IMAGE >> 2] = ramdisk_address;
+    bzimage32[LINUX_BOOT_HDR_RAMDISK_SIZE >> 2] = ramdisk_size;
+
+    dbg_assert(base_ptr + real_mode_kernel.length < 0xA0000);
+
+    mem8.set(real_mode_kernel, base_ptr);
+    mem8.set(protected_mode_kernel, KERNEL_HIGH_ADDRESS);
+
+    return {
+        option_rom:
+        {
+            name: "genroms/kernel.bin",
+            data: make_linux_boot_rom(real_mode_segment, heap_end),
+        }
+    };
+}
+
+function make_linux_boot_rom(real_mode_segment, heap_end)
+{
+    const SIZE = 0x200;
+    const data8 = new Uint8Array(0x100);
+    const data16 = new Uint16Array(data8.buffer);
+
+    data16[0] = 0xAA55;
+    data8[2] = SIZE / 0x200;
+
+    let i = 3;
+    data8[i++] = 0xFA; // cli
+    data8[i++] = 0xB8; // mov ax, real_mode_segment
+    data8[i++] = real_mode_segment >> 0;
+    data8[i++] = real_mode_segment >> 8;
+    data8[i++] = 0x8E; data8[i++] = 0xC0; // mov es, ax
+    data8[i++] = 0x8E; data8[i++] = 0xD8; // mov ds, ax
+    data8[i++] = 0x8E; data8[i++] = 0xE0; // mov fs, ax
+    data8[i++] = 0x8E; data8[i++] = 0xE8; // mov gs, ax
+    data8[i++] = 0x8E; data8[i++] = 0xD0; // mov ss, ax
+    data8[i++] = 0xBC; // mov sp, heap_end
+    data8[i++] = heap_end >> 0;
+    data8[i++] = heap_end >> 8;
+    data8[i++] = 0xEA; // jmp (real_mode_segment+0x20):0x0
+    data8[i++] = 0x00;
+    data8[i++] = 0x00;
+    data8[i++] = real_mode_segment + 0x20 >> 0;
+    data8[i++] = real_mode_segment + 0x20 >> 8;
+
+    dbg_assert(i < SIZE);
+
+    const checksum_index = i;
+    data8[checksum_index] = 0;
+
+    let checksum = 0;
+    for(let i = 0; i < data8.length; i++) checksum += data8[i];
+    data8[checksum_index] = -checksum;
+
+    return data8;
+}
